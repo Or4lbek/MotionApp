979d3df4970a79309f29357f9fdc367b10eabde8
Aslan <you@example.com>
2021-12-11 13:52:28 +0600

    splash screen update

--- app/src/main/java/com/sapar/motionapplication/SplashScreenActivity.kt
+++ app/src/main/java/com/sapar/motionapplication/SplashScreenActivity.kt
@@ -3,13 +3,19 @@ package com.sapar.motionapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreenActivity : AppCompatActivity() {
+
+    private val splashScreentimeOut : Long = 2000
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        val intent: Intent = Intent(this,RegistrationActivity::class.java)
-        startActivity(intent)
-        finish()
+        Handler().postDelayed({
+            startActivity(Intent(this, RegistrationActivity::class.java))
+            finish()
+        },splashScreentimeOut)
     }
 }
